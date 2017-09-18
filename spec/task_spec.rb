dc6dcde6bc15c9ef317b33ce01a08840d0368a5d
Dan Nollette and Erik Zakrzewski <dan@>
2017-09-18 13:10:25 -0700

    get project caught up to the lesson version.

---
+++ spec/task_spec.rb
@@ -0,0 +1,36 @@
+require("spec_helper")
+
+describe(Task) do
+  describe(".all") do
+    it("is empty at first") do
+      expect(Task.all()).to(eq([]))
+    end
+  end
+  describe("#description") do
+    it("lets you read the description out") do
+    test_task = Task.new({:description => "learn SQL", list_id: 1, due_date: '1970-01-01 00:00:01'})
+    expect(test_task.description()).to(eq("learn SQL"))
+    end
+  end
+
+  describe("#list_id") do
+    it("lets you read the list ID out") do
+      test_task = Task.new({description: "learn SQL", list_id: 1, due_date: '1970-01-01 00:00:01'})
+      expect(test_task.list_id()).to(eq(1))
+    end
+  end
+  describe("#==") do
+    it("is the same task if it has the same description") do
+      task1 = Task.new({:description => "learn SQL", list_id: 1, due_date: '1970-01-01 00:00:01'})
+      task2 = Task.new({:description => "learn SQL", list_id: 1, due_date: '1970-01-01 00:00:01'})
+      expect(task1).to(eq(task2))
+    end
+  end
+  describe("#save") do
+    it("adds a task to the array of saved tasks") do
+      test_task = Task.new({:description => "learn SQL", list_id: 1, due_date: '1970-01-01 00:00:01'})
+      test_task.save()
+      expect(Task.all()).to(eq([test_task]))
+    end
+  end
+end
